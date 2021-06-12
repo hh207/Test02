2b6811246ca321676389beca6d86dda77e52455a
hh207 <3224396114@qq.com>
2021-06-12 22:05:41 +0800

    Initial commit

---
+++ src/com/zb/test/Test.java
@@ -0,0 +1,14 @@
+package com.zb.test;
+
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        System.out.println(123456);
+    }
+
+    public static void main(String[] args) {
+        int i = 1000;
+        System.out.println(i);
+    }
+}
